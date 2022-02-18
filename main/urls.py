8a94731adc50a469952f399f9a1cb361f2b770bf
Pus9vGang <79223957+Andrew140m@users.noreply.github.com>
2022-02-18 23:17:53 +0300

    5

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.newspage, name='newspage'),
+    path('main.html', views.newspage, name='newspage'),
+    path('offer', views.offerpage, name='offerpage'),
+	path('warning', views.warning, name='usermode'),
+	path('donate', views.donate, name='usermode'),
+	# path('about', views.about, name='usermode'),
+]
